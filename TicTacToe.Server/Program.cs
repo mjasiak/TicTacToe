325adb8c633d45b26ace44326adf3e2e316769c2
mjasiak <michal.jasiak148@o2.pl>
2018-11-28 20:17:02 +0100

    Changes

--- TicTacToe.Server/Program.cs
+++ TicTacToe.Server/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Net;
 
 namespace TicTacToe.Server
 {
     class Program
     {
+        private const int _listeningPort = 50000;
         static void Main(string[] args)
         {
+            var listeningIP = IPAddress.Parse("127.0.0.1");
+            var listener = new TcpListener(listeningIP, _listeningPort);
+            listener.Start();
+            while(true){
+                var client = listener.Accept
+            }
             Console.WriteLine("Hello World!");
         }
     }
