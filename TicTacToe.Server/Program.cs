28e980d4994c5ff5e5bf54c1037d2f1abd5b1728
Michal Jasiak <michal.jasiak@pl.abb.com>
2018-11-29 16:53:33 +0100

    name misspell correction

--- TicTacToe.Server/Program.cs
+++ TicTacToe.Server/Program.cs
@@ -10,16 +10,16 @@ namespace TicTacToe.Server {
     class Program {
         private const int _listeningPort = 50000;
         private static readonly ITcpServerManager _tcpServerManager;
-        private static readonly ITcpListenerProvider _tcpListenerProvicer;
+        private static readonly ITcpListenerProvider _tcpListenerProvider;
 
         static Program () {
             var kernel = new StandardKernel (new Bindings ());
             _tcpServerManager = kernel.Get<ITcpServerManager>();
-            _tcpListenerProvicer = kernel.Get<ITcpListenerProvider>();
+            _tcpListenerProvider = kernel.Get<ITcpListenerProvider>();
         }
         static void Main (string[] args) {          
             var listeningIP = IPAddress.Parse ("127.0.0.1");
-            var server = _tcpListenerProvicer.CreateListener(listeningIP, _listeningPort);
+            var server = _tcpListenerProvider.CreateListener(listeningIP, _listeningPort);
             _tcpServerManager.Start(server);
             _tcpServerManager.Listen(server);
         }
